8aded535333797c5ac4b1c2843dd3b63bc23195e
chyci <chyciayu@gmail.com>
2024-08-13 17:30:25 +0700

    navbar update

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -19,55 +19,14 @@
               </div>
               <!-- /Search -->
 
-              <ul class="navbar-nav flex-row align-items-center ms-auto">
+              <ul class="navbar-nav flex-row align-items-center ms-auto gap-2">
                 <!-- Place this tag where you want the button to render. -->
-                
-
-                <!-- User -->
-                <li class="nav-item navbar-dropdown dropdown-user dropdown">
-                  <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
-                    <div class="avatar avatar-online">
-                      <img src="{{asset('/img/avatars/8.jpeg')}}" alt class="w-px-40 h-auto rounded-circle" />
-                    </div>
-                  </a>
-                  <ul class="dropdown-menu dropdown-menu-end">
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        <div class="d-flex">
-                          <div class="flex-shrink-0 me-3">
-                            <div class="avatar avatar-online">
-                              <img src="{{asset('/img/avatars/8.jpeg')}}" alt class="w-px-40 h-auto rounded-circle" />
-                            </div>
-                          </div>
-                          <div class="flex-grow-1">
-                            <span class="fw-medium d-block">{{ Auth::user()->name }}</span>
-                            <small class="text-muted"> {{ Auth::user()->role }}</small>
-                          </div>
-                        </div>
-                      </a>
-                    </li>
-                    <li>
-                      <div class="dropdown-divider"></div>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        <i class="bx bx-cog me-2"></i>
-                        <span class="align-middle">Settings</span>
-                      </a>
-                    </li>
-                    <li>
-                      <div class="dropdown-divider"></div>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="{{route('logout')}}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                        <i class="bx bx-power-off me-2"></i>
-                        <span class="align-middle">Log Out</span>
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <!--/ User -->
+                <div class="d-flex">
+                  <div class="flex-grow-0 ">
+                    <span class="fw-medium d-block badge bg-label-secondary">{{ Auth::user()->name }}</span>
+                  </div>
+                </div>
+                <a href="javascript:void(0)" class=" btn btn-sm btn-outline-danger"><i class="bx bx-log-out-circle me-1"></i>Logout</a>
               </ul>
             </div>
           </nav>
